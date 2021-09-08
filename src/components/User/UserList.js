e0f903d44b46543626cbb33f33910b534f26782a
jorika <gogoladzeg321@gmail.com>
2021-09-08 17:21:24 +0400

    commit

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -4,7 +4,7 @@ import Card from "../UI/Card";
 import classes from "./UserList.module.css";
 const UserList = (props) => {
   return (
-    <Card ClassName={classes.users}>
+    <Card className={classes.users}>
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
